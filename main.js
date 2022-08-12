946a2b7144cc39071292b3a2ace21a46c76a4acd
zalwa charif <zalwacharif@gmail.com>
2022-08-12 20:23:58 +0200

    segundoCommit

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-// gsap.to(".list", { opacity: 1, duration: 2});
-
-// let list = document.querySelectorAll ('.ul');
-
-// let btn = document.querySelector ('.btn');
-
-// btn.addEventListener ('mouseover' =>{
-     
-// })
+gsap.to(".titulo", {opacity: 1, duration: 2});
 
 let datos = [
 {
@@ -49,14 +41,42 @@ let datos = [
 
     <div class="card text-center" style="width: 18rem;">
         <img src=${dato.img} class= "img__card" alt="Card image cap">
-        <div class="card-body">
+        <div class="card-body d-flex flex-column justify-content-around">
         <h5 class="card-title">${dato.producto}</h5>
         <p class="~${dato.caracteristicas}></p>
-        <h6 class="card-title">${dato.precio}</h6><br>
-        <a href="#" class="btn btn-dark text-white">Buy now</a>
+        <h6 class="card-title ">${dato.precio}</h6><br>
+        <a href="#" class="btn btn-dark text-white my-3">Buy now</a>
          </div>
     </div>
     `
     cards.appendChild(cols);
 
  });
+
+//  Login
+let login = document.querySelector('#toggleLogin');
+ login.addEventListener('click', () =>{
+  let = loginSide = document.querySelector('#loginSide');
+  loginSide.classList.add('active_login');
+ });
+
+ let exitLogin = document.querySelector('#exitLogin');
+ exitLogin.addEventListener('click', () =>{
+    let = loginSide = document.querySelector('#loginSide');
+    loginSide.classList.remove('active_login');
+   });
+
+//    Register
+let register = document.querySelector('#toggleRegister');
+  register.addEventListener('click', () =>{
+   let = registerSide = document.querySelector('#registerSide');
+   registerSide.classList.add('active_login');
+ });
+
+ let exitRegister = document.querySelector('#exitRegister');
+  exitRegister.addEventListener('click', () =>{
+    let = loginSide = document.querySelector('#registerSide');
+    registerSide.classList.remove('active_login');
+});
+
+ 
